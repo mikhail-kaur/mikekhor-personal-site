2e9eff72691cac823441f93b259e5600f0a4f82b
mikhail-kaur <enghoekhor@yahoo.com>
2025-02-10 09:04:27 -0800

    Set up GitHub Pages with custom domain (#5)
    * Add GitHub Pages configuration
    
    * Add CNAME file for custom domain
    
    * package lock
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: Mike Khor <khorenghoe@gmail.com>

--- src/components/BlogPostScroll.tsx
+++ src/components/BlogPostScroll.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { CardContent, Typography, Grid, Card } from "@mui/material";
-import ReactMarkdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
 import { BlogPost } from "../utils/blogUtils";
 
 export default function BlogPostScroll({ posts, category, navigate }: { posts: BlogPost[], category: string, navigate: (path: string) => void }) {
